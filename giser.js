6a97ca5bbcb0ae0c5702fe838e4cfca213cd82fd
huangli <849151701@qq.com>
2018-04-25 16:46:15 +0800

    add giser.js

---
+++ giser.js
@@ -0,0 +1,3 @@
+var giserDiv = document.createElement('div');
+giserDiv.innerHTML = '<a href="http://giser.xyz/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="http://giser.xyz/demo/giser.png" alt="GISer空间"></a>';
+document.body.appendChild(giserDiv);
