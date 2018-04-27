36b3410ff04d1b009882a529a7f4b50723a43c69
huangli <849151701@qq.com>
2018-04-27 21:50:22 +0800

    change to https

--- giser.js
+++ giser.js
@@ -1,3 +1,1 @@
-var giserDiv = document.createElement('div');
-giserDiv.innerHTML = '<a href="http://giser.xyz/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="http://giser.xyz/demo/giser.png" alt="GISer空间"></a>';
-document.body.appendChild(giserDiv);
+;(function(){var giser=document.createElement('div');giser.innerHTML='<a href="https://giser.xyz/"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://giser.xyz/demo/giser.png" alt="GISer空间"></a>';document.body.appendChild(giser)})();
